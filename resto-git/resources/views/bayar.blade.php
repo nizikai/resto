bd6c8e15844aa6743eaacfbe189e56f61723429e
nizikai <nprasetyo@student.ciputra.ac.id>
2022-05-14 13:49:53 +0700

    Co-authored-by: BillyAnthonyB <BillyAnthonyB@users.noreply.github.com>

--- resto-git/resources/views/bayar.blade.php
+++ resto-git/resources/views/bayar.blade.php
@@ -10,7 +10,47 @@
 </head>
 <body>
 <section id = "bayar">
-
+    <div class="title">
+        <h1>Bayar</h1>
+        <h2>Meja 1.12</h2>
+    </div>
+    <table>
+        <tr>
+            <th>Menu</th>
+            <th>Jumlah</th>
+            <th>Harga</th>
+        </tr>
+        <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+        </tr>
+        <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+        </tr>
+        <tr>
+            <td>Ernst Handel</td>
+            <td>Roland Mendel</td>
+            <td>Austria</td>
+        </tr>
+        <tr>
+            <td>Island Trading</td>
+            <td>Helen Bennett</td>
+            <td>UK</td>
+        </tr>
+        <tr>
+            <td>Laughing Bacchus Winecellars</td>
+            <td>Yoshi Tannamuri</td>
+            <td>Canada</td>
+        </tr>
+        <tr>
+            <td>Magazzini Alimentari Riuniti</td>
+            <td>Giovanni Rovelli</td>
+            <td>Italy</td>
+        </tr>
+</table>
 </section>
 </body>
 </html>
