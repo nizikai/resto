63488f9ae921a0932dec9016b434a94213fd2af7
nizikai <nprasetyo@student.ciputra.ac.id>
2022-05-16 21:15:00 +0700

    Co-authored-by: Inez-z <Inez-z@users.noreply.github.com> Co-authored-by: BillyAnthonyB <BillyAnthonyB@users.noreply.github.com>

---
+++ resto-git/resources/views/pesanan.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
+    <title>Pesanan</title>
+</head>
+<body>
+<section id = "pesanan">
+    <div class = "title">
+        <h1>Pesanan</h1>
+    </div>
+    <div class = "content">
+        <div class = "button">
+            <button type="submit" class="buttontambahpesanan">Terima Pesanan</button>
+        </div>
+    </div>
+</section>
+</body>
+</html>
