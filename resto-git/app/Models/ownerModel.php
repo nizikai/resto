9bb58ba2011bfba9ec0713e1469f2fc69f60a39b
nizikai <nprasetyo@student.ciputra.ac.id>
2022-05-23 16:35:04 +0700

    commit
    Co-authored-by: BillyAnthonyB <BillyAnthonyB@users.noreply.github.com>
    Co-authored-by: Inez-z <Inez-z@users.noreply.github.com>

--- resto-git/app/Models/ownerModel.php
+++ resto-git/app/Models/ownerModel.php
@@ -13,14 +13,24 @@ class ownerModel extends Model
     function get_editMeja() {
         $querysemuameja = "SELECT NO_MEJA FROM SAD_NICO.meja;";
         $executequerysemuameja= DB::select($querysemuameja);
+        return $executequerysemuameja;
+    }
+
+    //buat insert meja baru di page edit meja
+    function post_insertMeja($tboxMejaBaru) {
+        //TINGGAL GANTI QUERY INSERT MEJA BARU. NICO.
+        $cmd = "INSERT INTO customer( `ID_MEMBERSHIP`, `NAMA_CUSTOMER`, `ALAMAT`, `PHONE`, `EMAIL`, `PASSWORD`, `DELETE_CUSTOMER`) VALUES ('REGU', :nama,'-', '-', :email, :password_customer,'0')";
+
+        $result =DB::insert($cmd, $tboxMejaBaru);
+        return $result;
     }
 
     // ini login owner
     public function cekLoginowner($tboxLogin)
     {
         $queryCekLoginK = "SELECT count(*) is_exist ".
-                         "FROM laundry_service.karyawan ".
-                         "WHERE ID_KARYAWAN = :loginEmail AND PASSWORD = :loginPassword ;";
+                         "FROM SAD_NICO.admin_karyawan ".
+                         "WHERE EMAIL = :loginEmail AND PASSWORD = :loginPassword AND OWNER = '1' ;";
         $executeQueryCekLoginK = DB::select($queryCekLoginK, $tboxLogin);
         // dd($executeQueryCekLogin);
 
@@ -33,6 +43,19 @@ class ownerModel extends Model
             return $executeQueryCekLoginK;
         }
         return null;
+    }
+
+    // ini tambah admin
+    function post_insert($tboxinsertadmin) {
+
+        $queryupdate = "INSERT INTO admin_karyawan( `NAMA`, `PASSWORD`, `OWNER`, `EMAIL`, `STAFF`, `DEL_STATUS`) VALUES (':insertEmail', '','0', 'TBOXEMAIL', '1', '0')";
+        // $dataIdUpdate = [
+        //     'insertEmail' => $tboxinsertadmin['insertEmail'],
+        //     'insertPassword' => $tboxinsertadmin['insertPassword']
+        // ]; //declare biar bisa dipake di query
+        $executequeryupdate = DB::insert($queryupdate, $tboxinsertadmin);
+        // dd($executequeryupdate);
+        return $executequeryupdate;
     }
 }
 
